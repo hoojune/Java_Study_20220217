3e69f7ca4696b15051d5e7017f313041cf42f921
hoojune <adsfdg5469@naver.com>
2022-02-22 07:13:42 +0900

    연산자 수업내용

---
+++ JavaStudy_5469/src/a03_연산자/Opreation01.java
@@ -0,0 +1,13 @@
+package a03_연산자;
+
+public class Opreation01 {
+
+	public static void main(String[] args) {
+		int score = 10;
+				
+		System.out.println(score++);
+		System.out.println(score--);
+		System.out.println(score);
+	}
+
+}
