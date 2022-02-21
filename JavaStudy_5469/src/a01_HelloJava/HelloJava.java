1a323af5b15f23c1904a483a46b560f346dba0c4
hoojune <adsfdg5469@naver.com>
2022-02-22 07:10:52 +0900

    첫수업HelloJava출력해보기

---
+++ JavaStudy_5469/src/a01_HelloJava/HelloJava.java
@@ -0,0 +1,18 @@
+package a01_HelloJava;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("성호준");
+		System.out.println("java.hello");
+		System.out.println("");
+		
+		System.out.print("이름:");
+		System.out.println("성호준");
+		System.out.print("연락처:");
+		System.out.print("010-3822-5469\n");
+		System.out.print("주소:");
+		System.out.print("부산광역시");
+				
+	}
+}
